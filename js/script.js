7c2c24bcf4ce0afcf2f7908b0bfaee5cb613316d
FluxZKClient <eaedoongg@gmail.com>
2025-10-26 10:30:14 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.querySelector('.btn');
+    btn.addEventListener('click', () => {
+        btn.classList.add('clicked');
+        setTimeout(() => btn.classList.remove('clicked'), 300);
+    });
+});
